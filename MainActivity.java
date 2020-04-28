a37f4c8dbf8fc72ee144fa52eda9bca95429bee7
syota-7 <act20191115@gmail.com>
2020-04-28 13:35:18 +0900

    SmartphoneApp

---
+++ MainActivity.java
@@ -0,0 +1,153 @@
+package com.s20200229.Study;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView countLabel;
+    private TextView questionLabel;
+    private Button answerBtn1;
+    private Button answerBtn2;
+    private Button answerBtn3;
+    private Button answerBtn4;
+
+    private String rightAnswer;
+    private int rightAnswerCount = 0;
+    private int quizCount = 1;
+    static final private int QUIZ_COUNT = 5;
+
+
+    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+
+    String quizData[][] = {
+            // {"問題", "正解", "選択肢１", "選択肢２", "選択肢３"}
+            {"2+3=", "5", "4", "6", "7"},
+            {"7+1", "8", "9", "10", "11"},
+            {"5+4=", "9", "7", "8", "10"},
+            {"2+2=", "4", "5", "6", "3"},
+            {"6+8=", "14", "13", "12", "11"},
+            {"9+9=", "18", "17", "19", "20"},
+            {"4+3=", "7", "6", "8", "9"},
+            {"4+8=", "12", "11", "13", "14"},
+            {"1+9=", "10", "11", "12", "13"},
+            {"5+5=", "10", "9", "8", "7"},
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        countLabel = findViewById(R.id.countLabel);
+        questionLabel = findViewById(R.id.questionLabel);
+        answerBtn1 = findViewById(R.id.answerBtn1);
+        answerBtn2 = findViewById(R.id.answerBtn2);
+        answerBtn3 = findViewById(R.id.answerBtn3);
+        answerBtn4 = findViewById(R.id.answerBtn4);
+
+        // ここから追加
+        // quizDataからクイズ出題用のquizArrayを作成する
+        for (int i = 0; i < quizData.length; i++) {
+
+            // 新しいArrayListを準備
+            ArrayList<String> tmpArray = new ArrayList<>();
+
+            // クイズデータを追加
+            tmpArray.add(quizData[i][0]);  // 計算クイズ
+            tmpArray.add(quizData[i][1]);  // 正解
+            tmpArray.add(quizData[i][2]);  // 選択肢１
+            tmpArray.add(quizData[i][3]);  // 選択肢２
+            tmpArray.add(quizData[i][4]);  // 選択肢３
+
+            // tmpArrayをquizArrayに追加する
+            quizArray.add(tmpArray);
+        }
+
+        showNextQuiz();
+
+    }
+
+    public void showNextQuiz() {
+        // クイズカウントラベルを更新
+        countLabel.setText("Q" + quizCount);
+
+        // ランダムな数字を取得
+        Random random = new Random();
+        int randomNum = random.nextInt(quizArray.size());
+
+        // randomNumを使って、quizArrayからクイズを一つ取り出す
+        ArrayList<String> quiz = quizArray.get(randomNum);
+
+        // 問題文を表示
+        questionLabel.setText(quiz.get(0));
+
+        // 正解をrightAnswerにセット
+        rightAnswer = quiz.get(1);
+
+        // クイズ配列から問題文を削除
+        quiz.remove(0);
+
+        // 正解と選択肢３つをシャッフル
+        Collections.shuffle(quiz);
+
+        // 回答ボタンに正解と選択肢３つを表示
+        answerBtn1.setText(quiz.get(0));
+        answerBtn2.setText(quiz.get(1));
+        answerBtn3.setText(quiz.get(2));
+        answerBtn4.setText(quiz.get(3));
+
+        // このクイズをquizArrayから削除
+        quizArray.remove(randomNum);
+    }
+
+    public void checkAnswer(View view) {
+
+        // どの回答ボタンが押されたか
+        Button answerBtn = findViewById(view.getId());
+        String btnText = answerBtn.getText().toString();
+
+        String alertTitle;
+        if (btnText.equals(rightAnswer)) {
+            alertTitle = "せいかい!";
+            rightAnswerCount++;
+        } else {
+            alertTitle = "まちがい...";
+        }
+
+        // ダイアログを作成
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle);
+        builder.setMessage("答え : " + rightAnswer);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (quizCount == QUIZ_COUNT) {
+                    // 結果画面へ移動
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount);
+                    startActivity(intent);
+
+
+                } else {
+                    quizCount++;
+                    showNextQuiz();
+                }
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+}
---
+++ ResultActivity.java
@@ -0,0 +1,39 @@
+package com.s20200229.Study;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView resultLabel = findViewById(R.id.resultLabel);
+        TextView totalScoreLabel = findViewById(R.id.totalScoreLabel);
+        int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
+        SharedPreferences prefs = getSharedPreferences("quizApp", Context.MODE_PRIVATE);
+        int totalScore = prefs.getInt("totalScore", 0);
+        totalScore += score;
+
+        resultLabel.setText(score + " / 5");
+        totalScoreLabel.setText("トータルスコア : " + totalScore);
+
+        // トータルスコアを保存
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("totalScore", totalScore);
+        editor.apply();
+    }
+
+    public void returnTop(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
